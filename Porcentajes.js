99ffae371b9b9c1f0eba18640f39261deb933381
Agustin Molina <agustomasmolina@hotmail.com>
2023-03-23 09:31:10 -0300

    Manejo de objetos con cupones

--- Porcentajes.js
+++ Porcentajes.js
@@ -4,31 +4,72 @@ const resultadoDesc = document.querySelector('#result');
 buttonResult.addEventListener('click', () => capturaResult());
 
 const capturaResult = () => {
-    const numero1 = document.querySelector('#descuento').value;
-    const numero2 = document.querySelector('#precio').value;
-    return porcentajeDescuento(numero1, numero2);
+    const cuponDescuento = document.querySelector('#cupon').value;
+    const precioProducto = document.querySelector('#precio').value;
+    return porcentajeDescuento(cuponDescuento, precioProducto);
 }
 
-function porcentajeDescuento(descuento, precio) {
-    console.log(descuento, precio);
-    let desc = descuento;
+const cuponObj = {
+    'agusmol': 15,
+    'moli99': 25,
+    'genialidad': 30,
+    'monstruo': 35,
+}
+
+function porcentajeDescuento(cuponDescuento, precio) {
+    console.log(cuponDescuento, precio);
+    let cupon = cuponDescuento;
     let price = precio;
     let resultado;
 
-    console.log({precio , descuento});
-
-    if (!price || !descuento) {
+    if (!price || !cupon) {
 
         resultadoDesc.innerText = 'Porfavor llenar los datos'
         return;
     } 
-    if (descuento > 100) {
 
-        resultadoDesc.innerText = 'El descuento es mayor a 100'
+    let discount;
+
+    if (cuponObj[cupon]) {   // Se pregunta si dentro del objeto existe algun elemento 
+                            //  que el cupo que escribieron los usuarios en el input en el HTML.
+
+        discount = cuponObj[cupon]; // Descuento es igual al elemento obj. y al input que genera el usuario.
+
+    }else {
+
+        resultadoDesc.innerText = 'El cupon no es valido'
         return;
     }
 
-        resultado = (price * (100 - descuento)) / 100;
+
+
+   /*  switch (cupon) {
+        case 'Molina_cupon':
+            descuento = 30;
+            break;
+        case "no_sigas_las_reglas":
+            descuento = 25;
+            break;
+
+        default:
+            resultadoDesc.innerText = 'El cupon no es valido'
+            return;         
+    } */
+/*
+       let descuento;
+    
+    if (cupon == "Molina_cupon") {
+        descuento = 30;
+    } else if(cupon == "no_sigas_las_reglas") {
+        descuento = 25;
+    } else {
+        resultadoDesc.innerText = 'El cupon no es valido'
+        return;
+    } */
+
+    console.log({precio , discount});
+
+        resultado = (price * (100 - discount)) / 100;
     
     resultadoDesc.innerText = 'El nuevo precio con descuento es $' + resultado;
     } 
